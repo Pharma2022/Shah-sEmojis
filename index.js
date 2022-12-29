c5b3728c3d5e890df587a82964dce726ebda32fe
Pharma2022 <111680056+Pharma2022@users.noreply.github.com>
2022-12-29 18:25:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -26,11 +26,3 @@ document.addEventListener("click", e =>{
     
 })
 
-document.getElementById("btn").addEventListener("click",function gfgMenu() 
-            {const nav = document.querySelector('.nav');
-  
-            if (nav.style.display === "none") 
-            {nav.style.display = "block";}
-            else {nav.style.display = "none";}
-              }
-        )// javascript
